a504a62b090f376d91eff8b43060b0d342679ffe
guozeping <mr_guozeping@163.com>
2021-04-08 21:26:26 +0800

    fix:(String):alter string dir struct

--- 1-基础/基本变量类型/02-String/内建函数/01-增/002-zfill.py
+++ 1-基础/基本变量类型/02-String/内建函数/01-增/002-zfill.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 __author__ = 'gzp'
 str = "this is string example from runoob....wow!!!"
-print ("str.zfill : ",str.zfill(40))
-print ("str.zfill : ",str.zfill(50))
-print(str.rfind())
+print (str.zfill(40))
+print (str.zfill(50))
+
 
