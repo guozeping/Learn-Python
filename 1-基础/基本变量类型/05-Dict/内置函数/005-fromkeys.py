a3475338846e42adf240f6e25bf8be04e5b5ef2b
guozeping <mr_guozeping@163.com>
2021-04-08 21:55:47 +0800

    fix:(List and Dict):add dir struct

---
+++ 1-基础/基本变量类型/05-Dict/内置函数/005-fromkeys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env  python
+# --*--coding:utf-8 --*--
+seq = ('Google', 'Runoob', 'Taobao')
+dict = dict.fromkeys(seq)
+print(dict)
+
+dict = dict.fromkeys(seq,10)
+print(dict)
---
+++ 1-基础/基本变量类型/05-Dict/内置函数/007-setdefault.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env  python
+# --*--coding:utf-8 --*--
+dict = {'runoob': '菜鸟教程', 'google': 'Google 搜索'}
+print(dict.setdefault('runoob'))
+print(dict.setdefault('Taobao','淘宝'))
