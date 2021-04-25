97e5cf63fba9a3c5dc6050184d505ea5099111dd
guozeping <mr_guozeping@163.com>
2021-04-25 11:30:57 +0800

    feature:(file):add files

--- 1-基础/模块/File/fileobj/003-fileno.py
+++ 1-基础/模块/File/fileobj/003-fileno.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env  python
 # --*--coding:utf-8 --*--
+with open('../test.txt') as fp:
+    line = fp.readline()
+    print(line)
--- 1-基础/模块/File/fileobj/007-readlines.py
+++ 1-基础/模块/File/fileobj/007-readlines.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env  python
 # --*--coding:utf-8 --*--
+with open('../test.txt',errors='debug') as fp:
+    lines = fp.readlines()
+    print(lines)
